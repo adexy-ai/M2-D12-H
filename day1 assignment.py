1d05188057aa0420d120d522dc410a2dd9d8f124
Aderiye <69057307+adexy-ai@users.noreply.github.com>
2022-08-20 19:33:18 +0300

    this is to refresh python

---
+++ day1 assignment.py
@@ -0,0 +1,12 @@
+# 1. Create a greeting for your program.
+greeting = input("Hello welcome to Python basic programing!")
+
+# 2. Ask the user for the city that they grew up in.
+city = input("what city did you grow up?\n")
+# 3. Ask the user for the name of a pet.
+pet_name = input("what is the name of your pet\n")
+# 4. Combine the name of their city and pet and show them their band name.
+print(
+    f'The name of your city is {city}, and the name of your pet is {pet_name}\n')
+# 5. Make sure the input cursor shows on a new line, see the example at:
+#   https://replit.com/@appbrewery/band-name-generator-end
